4fa6bc327fdda64ee5cbafa66e34a6a2b78a78d1
Menelik Tefera <menelikworku@gmail.com>
2017-08-16 01:20:12 -0400

    changed padding on OL

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Falcao15",
+  password: "",
   database: "burgers_db"
   });
 }
